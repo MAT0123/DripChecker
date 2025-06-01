6a5121eb67377c2211eb677af7a7128469fe794a
Matthew <matthewaureliustjoa@gmail.com>
2025-06-01 00:47:31 -0400

    Initial Commit

---
+++ Drip Check/Drip_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drip_CheckApp.swift
+//  Drip Check
+//
+//  Created by Matthew Tjoa on 2025-06-01.
+//
+
+import SwiftUI
+
+@main
+struct Drip_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
